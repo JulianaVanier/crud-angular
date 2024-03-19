d8818e95aff66c2fce0be3ab3f4ae76b9b230647
Juliana Silva <jusilva@newtrax.com>
2024-03-19 11:36:58 -0400

    module courses and router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes } from '@angular/router';
 
+const routes: Routes = [
+  { path:'', pathMatch: 'full', redirectTo: 'courses' },
 
+  //lazyload
+  {
+    path: 'courses',
+    loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
+  }
+];
 
 @NgModule({
   declarations: [],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,8 @@
+//app.component.ts is the source file that describes the app-root component.
+//This is the top-level Angular component in the app. A component is the basic building block of an Angular application.
+//The component description includes the component's code, HTML template, and styles, which can be described in this file, or in separate files.
+//In this app, the styles are in a separate file while the component's code and HTML template are in this file.
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
