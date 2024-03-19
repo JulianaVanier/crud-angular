34452fef17e1dc34b7d6b96d888c79877fc1e3db
Juliana Silva <jusilva@newtrax.com>
2024-03-19 13:23:41 -0400

    fixed router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes } from '@angular/router';
-
-const routes: Routes = [
-  { path:'', pathMatch: 'full', redirectTo: 'courses' },
-
-  //lazyload
-  {
-    path: 'courses',
-    loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
-  }
-];
 
 @NgModule({
   declarations: [],
--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
-import { CoursesComponent } from './courses/courses.component';
 
 
 @NgModule({
-  declarations: [
-    CoursesComponent
-  ],
+  declarations: [],
   imports: [
     CommonModule,
     CoursesRoutingModule
