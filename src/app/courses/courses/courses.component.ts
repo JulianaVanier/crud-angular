f896992c32e6b1d995945f6cb80f853bd439a8a8
Juliana Silva <jusilva@newtrax.com>
2024-03-19 15:21:35 -0400

    created service class

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,23 +1,27 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AppMaterialModule } from '../../shared/app-material/app-material.module';
 
 import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [AppMaterialModule],
+  imports: [AppMaterialModule, HttpClientModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent implements OnInit {
-  courses: Course[] = [
-    { _id: '1', name:'Angular', category: 'Front-end'}
-  ];
+  courses: Course[] = [];
   displayedColumns = ['name', 'category'];
 
-  constructor() {
+  //coursesService: CoursesService;
 
+  constructor(private coursesService: CoursesService) {
+    //this.coursesService = new CoursesService();
+    this.courses = this.coursesService.list();
   }
   ngOnInit(): void {
 
